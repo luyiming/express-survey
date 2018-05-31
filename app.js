4e033e44cdae609f790ceeab80096e1e781fa40d
luyiming <luyimingchn@gmail.com>
2018-05-31 11:16:02 +0800

    change log format

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger(':remote-addr [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
